08aed51f40e1a98393ae84ad2e3ae7936d87ce29
Đoàn Văn Nguyên <nguyendv.it4@vnpt.vn>
2020-12-09 16:24:40 +0700

    first commit

---
+++ src/HelloWorld.js
@@ -0,0 +1,10 @@
+function HelloWorld() {
+  return (
+    <div>
+        <h1>HelloWorld</h1>
+        <p>xin chào các bạn</p>
+    </div>
+  );
+}
+
+export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import HelloWorld from './HelloWorld';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>,
   document.getElementById('root')
 );
